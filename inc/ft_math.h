d60b2539f08d31360df891829996b8f0a46e2a43
dda-cunh <dda-cunh@student.42lisboa.com>
2024-05-22 19:44:33 +0100

    sync

--- inc/ft_math.h
+++ inc/ft_math.h
@@ -6,7 +6,7 @@
 /*   By: dda-cunh <dda-cunh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 17:17:44 by dda-cunh          #+#    #+#             */
-/*   Updated: 2024/05/22 18:17:41 by dda-cunh         ###   ########.fr       */
+/*   Updated: 2024/05/22 19:44:25 by dda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,16 +40,27 @@ typedef struct s_ray3
 	t_vec3		direction;
 }	t_ray3;
 
-t_point3	point3_plus_vec3(t_point3 point, t_vec3 vector);
-double		quadratic_smallest_pos(double a, double b, double c);
-double		clamp_value(double value, double min, double max);
-double		point3_distance_point3(t_point3 p1, t_point3 p2);
-double		pythagorean_theorem(double a, double b);
+/* ************************************************************************** */
+/*                                     VEC                                    */
+/* ************************************************************************** */
 double		vec3_dot_product(t_vec3 a, t_vec3 b);
 t_vec3		vec3_from_points(t_point3 start, t_point3 end);
 t_vec3		scale_vec3(t_vec3 vector, double scalar);
 t_vec3		vec3_cross_product(t_vec3 a, t_vec3 b);
 t_vec3		normalize_vec3(t_vec3 vector);
 t_vec3		vec3_sub(t_vec3 a, t_vec3 b);
+
+/* ************************************************************************** */
+/*                                    POINT                                   */
+/* ************************************************************************** */
+t_point3	point3_plus_vec3(t_point3 point, t_vec3 vector);
+double		point3_distance_point3(t_point3 p1, t_point3 p2);
+
+/* ************************************************************************** */
+/*                                   COMMON                                   */
+/* ************************************************************************** */
+double		quadratic_smallest_pos(double a, double b, double c);
+double		clamp_value(double value, double min, double max);
+double		pythagorean_theorem(double a, double b);
 
 #endif
